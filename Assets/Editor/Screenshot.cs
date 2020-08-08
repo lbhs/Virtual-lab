1232b48602e6fa10aebb77898b1fc7785cdbf319
ssogo <43896422+sgsogo@users.noreply.github.com>
2020-08-08 12:54:34 -0700

    Treasure Chest asset added

---
+++ Assets/Editor/Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+[ExecuteInEditMode]
+public class Screenshot : MonoBehaviour {
+
+
+	public static string screenshotPath = "C:\\UnityScreenshots\\screenshot.png";
+
+	[MenuItem("Utils/Create Screenshot")]
+	public static void CreateScreenshot()
+	{
+		ScreenCapture.CaptureScreenshot(screenshotPath);
+	}
+}
