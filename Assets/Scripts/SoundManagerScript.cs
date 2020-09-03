5d6082b406a964da89aab7cafce4114a247900bc
bennyd87708 <bennyd87708@gmail.com>
2020-09-03 15:20:11 -0700

    initial sound framework

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+	public static AudioClip fizzSound;
+	public static AudioClip pourSound;
+	static AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+		fizzSound = Resources.Load<AudioClip>("Sounds/fizz");
+		pourSound = Resources.Load<AudioClip>("Sounds/pour");
+    }
+
+    /*example for playing a sound anywhere in the code:
+	SoundManagerScript.audioSource.PlayOneShot(SoundManagerScript.fizzSound);
+	*/
+}
