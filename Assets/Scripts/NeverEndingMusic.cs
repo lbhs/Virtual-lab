07b2f1328c123f46dc897488573f74457264262a
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-09-14 16:06:52 -0700

    better sfx

---
+++ Assets/Scripts/NeverEndingMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeverEndingMusic : MonoBehaviour
+{
+    static NeverEndingMusic me;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (me == null)
+        {
+            me = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
